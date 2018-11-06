de653d0d40004ede0a01df93b9f695cdeeb1c87a
neocarlosgm1 <carlosg.munguia13@est.unanleon.edu.ni>
2018-11-06 13:22:15 -0600

    resuelto el 4

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -11,9 +11,22 @@ def max_2_sum arr
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if !arr.any?
+  	  return false
+  elsif arr.length == 1
+  	return false
+  else
+  	i= 0
+  	while i < arr.length - 2
+  		j =i + 1;
+  		while j < arr[i] + arr[j]
+  			if total ==n
+  				return true
+  			 end	
+          end
+      end
+   end
 end
-
 # Part 2
 
 def hello(name)
